7a55112d4b3819088b369957f0c8f145249202ea
MehdiMemmi <mehdi@dsply.fr>
2021-08-05 19:31:51 +0200

    anims en cours

--- assets/js/pop.js
+++
@@ -1,9 +0,0 @@
-const compFront = document.getElementById('#concFrontEnd');
-const compBack = document.getElementById('#concBackEnd');
-
-window.addEventListener('scroll', function(){
-
-    console.log(window.innerHeight);
-    console.log(window.scrollY);
-    console.log(document.body.offsetHeight);
-})
